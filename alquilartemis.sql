f414509af959d5d9356b20a53a18346c781b9a42
TotixT <santigolopezgarcia22@gmail.com>
2023-06-11 10:28:33 -0500

    (Cambio DB)

--- alquilartemis.sql
+++ alquilartemis.sql
@@ -23,7 +23,6 @@ CREATE TABLE cotizacion(
     Cotizacion_ID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     Empleados_ID INT NOT NULL,
     Clientes_ID INT NOT NULL,
-    Nombre_Cliente VARCHAR(60) COLLATE utf8mb4_spanish_ci NOT NULL,
     Fecha DATE NOT NULL,
     Foreign Key (Empleados_ID) REFERENCES empleados(Empleados_ID),
     Foreign Key (Clientes_ID) REFERENCES clientes(Clientes_ID)
